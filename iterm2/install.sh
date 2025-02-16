4d2652236fb65ebb239b18c4641183709bb30f38
Rémi Bardon <remi@remibardon.name>
2025-02-16 14:38:40 +0100

    🔧 Add iTerm2

---
+++ iterm2/install.sh
@@ -0,0 +1,4 @@
+echo 'Attention, you still need to:'
+echo 'iTerm2 > Settings > Load settings from a custom folder or URL: true'
+echo 'iTerm2 > Settings > Load settings from a custom folder or URL: "/Users/Shared/dotfiles/iterm2"'
+echo 'iTerm2 > Settings > Save changes: Automatically'
